6cd511f57d4b787b189b26e0034404e098088fa3
EmilyGG24308 <gon24308@uvg.edu.gt>
2025-05-16 12:05:41 -0600

    Update ControllerVistaRecomendacion.java

--- backend/src/main/java/com/ProyectoMaquillaje/controller/ControllerVistaRecomendacion.java
+++ backend/src/main/java/com/ProyectoMaquillaje/controller/ControllerVistaRecomendacion.java
@@ -9,7 +9,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.ProyectoMaquillaje.model.Blush;
 import com.ProyectoMaquillaje.model.Concelear;
+import com.ProyectoMaquillaje.model.Rimel;
+import com.ProyectoMaquillaje.repository.RepositorioBlush;
 import com.ProyectoMaquillaje.repository.RepositorioConcelear;
 
 @Controller
@@ -41,5 +44,44 @@ public class ControllerVistaRecomendacion {
 
         model.addAttribute("productos", correctores);
         return "results";  // Renderizar la plantilla results.html
+    }
+
+
+    @Autowired
+    private RepositorioBlush repositorioBlush;
+    @GetMapping("/recomendaciones-blush")
+    public String mostrarRecomendacionesBlush(
+            @RequestParam(required = false) String acabado,
+            @RequestParam(required = false) String presentacion,
+            @RequestParam(required = false) String tonoBlush,
+            Model model) {
+
+        List<Blush> blushes = repositorioBlush.recomendarPorRespuestas(
+                acabado != null ? acabado : "",
+                presentacion != null ? presentacion : "",
+                tonoBlush != null ? tonoBlush : ""
+        );
+
+        model.addAttribute("productos", blushes);
+        return "resultsBlush"; // Crea results-blush.html en templates
+    }
+
+    @Autowired
+    private com.ProyectoMaquillaje.repository.RepositorioRimel repositorioRimel;
+    @GetMapping("/recomendaciones-rimel")
+    public String mostrarRecomendacionesRimel(
+            @RequestParam(required = false) String color,
+            @RequestParam(required = false) String waterproof,
+            @RequestParam(required = false) String funcion,
+            Model model) {
+        boolean isWaterproof = Boolean.parseBoolean(waterproof);
+        List<Rimel> rimels = repositorioRimel.recomendarPorRespuestas(
+                color != null ? color : "",
+                isWaterproof,
+                funcion != null ? funcion : ""
+        );
+
+        model.addAttribute("productos", rimels);
+        return "resultsRimel"; // Crea results-rimel.html en templates
     }
 }
